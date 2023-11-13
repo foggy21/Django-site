ef9a28f6b552ebd759c86b5a7bce32fb292756ee
foggy21 <rashit.xaliullin@inbox.ru>
2023-11-13 22:16:29 +1000

    Added details for items

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -23,4 +23,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
+    path('items/', include("item.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
