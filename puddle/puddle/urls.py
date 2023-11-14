c5c89d3e27067e84993193a77fcc9dd80ac2b3e4
foggy21 <rashit.xaliullin@inbox.ru>
2023-11-14 11:47:25 +1000

    Added signup and login + added react

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -18,10 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 #Urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('react/', TemplateView.as_view(template_name='index.html')),
     path('', include("core.urls")),
     path('items/', include("item.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
